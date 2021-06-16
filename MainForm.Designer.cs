24d376f0095e0eec3f27642d59bf6ae1a761788e
hwangshkr <ruinahri@gmail.com>
2021-06-16 17:43:04 +0900

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace TarkovPriceViewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testB
+            // 
+            this.testB.Location = new System.Drawing.Point(627, 306);
+            this.testB.Name = "testB";
+            this.testB.Size = new System.Drawing.Size(145, 57);
+            this.testB.TabIndex = 0;
+            this.testB.Text = "TestButton";
+            this.testB.UseVisualStyleBackColor = true;
+            this.testB.Click += new System.EventHandler(this.testB_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.testB);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testB;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace TarkovPriceViewer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void testB_Click(object sender, EventArgs e)
+        {
+            Bitmap image = ScreenShot.Capture();
+            if (image != null)
+            {
+                image.Save("C:\\Users\\DreamChain\\Desktop\\test.png", ImageFormat.Png);
+            } else
+            {
+                Debug.WriteLine("image null");
+            }
+        }
+    }
+}
