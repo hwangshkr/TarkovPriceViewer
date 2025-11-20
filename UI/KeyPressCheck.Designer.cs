90785b3994d8491e67bb940dd4dd711f8f572cf3
JoShMiQueL <joshmiquel.cs@gmail.com>
2025-11-20 22:23:10 +0100

    refactor: update KeyPressCheck form designer properties
    - Update form properties: add ControlBox=false, FormBorderStyle=FixedToolWindow, Icon resource
    - Adjust control sizing and positioning (AutoScaleDimensions, ClientSize, label Location/Size)
    - Add form icon resource data to resx file

--- UI/KeyPressCheck.Designer.cs
+++ UI/KeyPressCheck.Designer.cs
@@ -21,7 +21,7 @@ namespace TarkovPriceViewer.UI
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
         /// <summary>
         /// Required method for Designer support - do not modify
